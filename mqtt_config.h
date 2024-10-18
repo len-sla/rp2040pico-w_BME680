66798cf9e716767e501bc9f326006f928594bfed
len-sla <68053910+len-sla@users.noreply.github.com>
2024-10-18 11:12:59 +0200

    Add files via upload

---
+++ mqtt_config.h
@@ -0,0 +1,9 @@
+#ifndef MQTT_CONFIG_H
+#define MQTT_CONFIG_H
+
+#define MQTT_SERVER_IP "192.168.x.x"
+#define MQTT_SERVER_PORT 1883
+#define MQTT_USER "user id"
+#define MQTT_PASSWORD "password"
+
+#endif // MQTT_CONFIG_H
